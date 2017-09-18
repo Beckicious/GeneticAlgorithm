52e37d79fe8b018a3fce08bdebe0df3cf4b3ef50
Simon Beck <simubeck@hotmail.com>
2017-09-18 18:12:41 +0200

    initial commit

---
+++ GeneticAlgorithm/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    class GeneticAlgorithm<DNA>
+    {
+
+        public double MutationRate { get; set; }
+        public int PopulationSize { get; set; }
+        public (DNA, double)[] CurrentGeneration { get; set; }
+        public int GenerationNumber { get; set; }
+
+        private Random rng;
+
+        public Func<DNA> InitialCreationFunction { get; set; }
+        public Func<DNA, double> CalculateFitnessFunction { get; set; }
+        public Func<(DNA, double), (DNA, double), double, DNA> BreedingFunction { get; set; }
+
+        public GeneticAlgorithm(Random random, double mutationRate = 0.01, int populationSize = 10)
+        {
+            this.MutationRate = mutationRate;
+            this.PopulationSize = populationSize;
+            this.CurrentGeneration = new (DNA, double)[this.PopulationSize];
+            this.GenerationNumber = 0;
+
+            rng = (random != null ? random : new Random());
+        }
+
+        public void PopulateFirstGeneration()
+        {
+            Parallel.For(0, PopulationSize, (index => CurrentGeneration[index] = (InitialCreationFunction(), -1)));
+        }
+
+        public void CalculateFitnessForCurrentGeneration()
+        {
+            Parallel.For(0, PopulationSize, (index => CurrentGeneration[index] = (CurrentGeneration[index].Item1, CalculateFitnessFunction(CurrentGeneration[index].Item1))));
+        }
+
+        public void GenerateNextGeneration()
+        {
+            (DNA, double)[] nextGeneration = new(DNA, double)[PopulationSize];
+
+            for (int index = 0; index < PopulationSize; index++)
+            {
+                nextGeneration[index] = (BreedingFunction(GetRandomDNA(), GetRandomDNA(), MutationRate), -1);
+            }
+
+            CurrentGeneration = nextGeneration;
+            GenerationNumber++;
+        }
+
+        private (DNA, double) GetRandomDNA()
+        {
+            double universalProbability = CurrentGeneration.Sum(DNATuple => DNATuple.Item2);
+
+            double randomNumber = rng.NextDouble() * universalProbability;
+
+            double sum = 0;
+            foreach ((DNA, double) DNATuple in CurrentGeneration)
+            {
+                if (randomNumber <= (sum += DNATuple.Item2))
+                {
+                    return DNATuple;
+                }
+            }
+
+            //should never be reached
+            return (InitialCreationFunction(), 0);
+        }
+    }
+}
---
+++ GeneticAlgorithm/GeneticAlgorithm/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    class Program
+    {
+        private static GeneticAlgorithm<string> ga;
+        private static char[] chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:.,ABCDEFGHIJKLMNOPQRSTUVWXYZ^& ".ToCharArray();
+        private const string Goal = "Simon Beck is a massive asshole LUL";
+        private static int NumOfGenes = Goal.Length;
+        private static Random rng;
+
+        static void Main(string[] args)
+        {
+            rng = new Random();
+
+            ga = new GeneticAlgorithm<string>(null, 0.01, 100);
+
+            ga.InitialCreationFunction = CreateNew;
+            ga.CalculateFitnessFunction = CalculateFitness;
+            ga.BreedingFunction = Breed;
+
+            ga.PopulateFirstGeneration();
+            ga.CalculateFitnessForCurrentGeneration();
+
+            //PrintCurrentGeneration();
+            PrintBestAndAverageFitness();
+            Console.ReadLine();
+
+            while(!IsGoalReached())
+            {
+                ga.GenerateNextGeneration();
+                ga.CalculateFitnessForCurrentGeneration();
+
+                PrintBestAndAverageFitness();
+                //Console.ReadLine();
+            }
+
+            Console.WriteLine($"\nGOAL REACHED IN GENERATION {ga.GenerationNumber} !!!\n");
+            PrintBestAndAverageFitness();
+            Console.ReadLine();
+        }
+
+        private static string CreateNew()
+        {
+            string s = "";
+
+            for (int i = 0; i < NumOfGenes; i++)
+            {
+                s += chars[rng.Next(chars.Length)];
+            }
+
+            return s;
+        }
+
+        private static double CalculateFitness(string DNA)
+        {
+            int fitness = 0;
+
+            for (int i = 0; i < NumOfGenes; i++)
+            {
+                if (DNA[i] == Goal[i]) fitness++;
+            }
+
+            return Math.Pow((double)fitness / DNA.Length,2);
+        }
+
+        private static string Breed((string, double) DNA1, (string, double) DNA2, double mutationRate)
+        {
+            string s = "";
+            double fitnessSum = DNA1.Item2 + DNA2.Item2;
+            double percentageOfSum = DNA1.Item2 / fitnessSum;
+            double cutPoint = (1 - mutationRate) * percentageOfSum + mutationRate;
+
+            for (int i = 0; i < NumOfGenes; i++)
+            {
+                double random = rng.NextDouble();
+
+                if (random < mutationRate) s += chars[rng.Next(chars.Length)];
+                else if (random < cutPoint) s += DNA1.Item1[i];
+                else s += DNA2.Item1[i];
+            }
+
+            return s;
+        }
+
+        private static bool IsGoalReached()
+        {
+            return ga.CurrentGeneration.Any(DNATupel => DNATupel.Item1 == Goal);
+        }
+
+        private static void PrintCurrentGeneration()
+        {
+            Console.WriteLine($"Generation : {ga.GenerationNumber}\n");
+
+            for (int i = 0; i < ga.PopulationSize; i++)
+            {
+                Console.WriteLine($"{ga.CurrentGeneration[i].Item1} : {ga.CurrentGeneration[i].Item2}");
+            }
+
+            Console.WriteLine("------------------------------------------");
+        }
+
+        private static void PrintBestAndAverageFitness()
+        {
+            string best = ga.CurrentGeneration.OrderByDescending(t => t.Item2).First().Item1;
+            double average = ga.CurrentGeneration.Average(t => t.Item2);
+
+            Console.WriteLine($"Generation : {ga.GenerationNumber}");
+            Console.WriteLine($"Best Solution = {best}");
+            Console.WriteLine($"Average fitness = {Math.Round(average, 2) * 100}%");
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
